b9dc6c0079677d498ab6f5e134bb43ada8257074
Nick Hopewell <nicholashopewell@gmail.com>
2021-05-26 10:04:47 -0400

    define serializer class for converting Listing objects to json

--- src/api/serializers.py
+++ src/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import ZillowEstimate
+from .models import ZillowEstimate, Listing
 
 class ZillowEstimateSerializer(serializers.ModelSerializer):
     
@@ -10,3 +10,16 @@ class ZillowEstimateSerializer(serializers.ModelSerializer):
                "zillow_id", "rentzestimate_amount", "rentzestimate_last_updated",
                "zestimate_amount", "zestimate_last_updated",
         )
+
+class ListingSerializer(serializers.ModelSerializer):
+
+    zillow_details = ZillowEstimateSerializer(read_only=True)
+
+    class Meta:
+        model = Listing
+        fields = (
+            "id", "area_unit", "bathrooms", "bedrooms", "home_size",
+            "home_type", "last_sold_date", "last_sold_price",
+            "link", "price", "property_size", "rent_price",
+            "zillow_details", "tax_value", "tax_year",
+            "year_built", "address", "city", "state", "zipcode",)
