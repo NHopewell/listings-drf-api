10de1e58ed10bae542b4993cf53df3fb457aaeb2
Nick Hopewell <nicholashopewell@gmail.com>
2021-06-02 16:46:01 -0400

    add test cases for Listing model

--- src/api/tests/test_models.py
+++ src/api/tests/test_models.py
@@ -1,19 +1,42 @@
 from django.test import TestCase
 
+from api.models import ZillowEstimate, Listing
+
 
 class TestListingModel(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        pass
+        Listing.objects.create(
+            address="test address",
+            city="test city",
+            state="XX",
+            zipcode="XXXXX"
+        )
 
 
     def setUp(self):
-        pass
+        self.listing = Listing.objects.get(address="test address")
 
 
-    def test_full_address(self):
-        pass
+    def test_add_zillow_details(self):
+        zdetails = ZillowEstimate.objects.create(
+                zillow_id = 9999999,
+                rentzestimate_amount = 2900,
+                rentzestimate_last_updated = "2018-08-07",
+                zestimate_amount = 800000,
+                zestimate_last_updated = "2018-08-07"
+        )
+
+        self.listing.zillow_details = zdetails
+
+        self.assertTrue(isinstance(self.listing.zillow_details, ZillowEstimate))
+
+
+    def test_complete_address(self):
+        complete_address = self.listing.complete_address
+
+        self.assertEqual(complete_address, "test address, test city, XX, XXXXX")
 
 
     def tearDown(self):
